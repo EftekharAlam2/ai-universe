c5bd2b72e3f38a2ee41c26d19d4da5d943c4ab95
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-03-03 00:18:33 +0600

    4th

--- connect.js
+++ connect.js
@@ -1,10 +1,68 @@
-const fetchData = () => {
+const fetchData = (dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.data.tools));
+    .then((data) => showData(data.data.tools, dataLimit));
 };
 
-const showData = (data) => {};
+const showData = (data, dataLimit) => {
+  const getClass = document.getElementById("insert-div");
+  getClass.innerHTML = "";
+  const seeMore = document.getElementById("see-more");
+  if (dataLimit && data.length > 6) {
+    seeMore.classList.add("d-none");
+  } else {
+    data = data.slice(0, 6);
+    seeMore.classList.remove("d-none");
+  }
+  data.forEach((element) => {
+    console.log(element);
 
+    getClass.innerHTML += `
+    <div class="col">
+          <div class="card">
+            <img src="${element.image}" style="width: 100%; height: 15rem" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Features</h5>
+              <ol id="list-add">
+              </ol>
+              <hr />
+              <div class="d-flex justify-content-between">
+                <div>
+                  <h5 class="card-title">${element.name}</h5>
+                  <i class="fa-solid fa-calendar-days"></i><span> ${element.published_in}</span>
+                </div>
+                <div class="my-auto">
+                  <button
+                    type="button"
+                    class="btn btn-light"
+                    style="border-radius: 50%"
+                  >
+                    <i class="fa-solid fa-arrow-right text-danger"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+  });
+  loadSpinner(false);
+};
+
+const loadSpinner = (isLoading) => {
+  const loadingId = document.getElementById("load-spinner");
+  if (isLoading) {
+    loadingId.classList.remove("d-none");
+  } else {
+    loadingId.classList.add("d-none");
+  }
+};
+
+const dataLimit = () => {
+  loadSpinner(true);
+  fetchData(10);
+};
+
+loadSpinner(true);
 fetchData();
